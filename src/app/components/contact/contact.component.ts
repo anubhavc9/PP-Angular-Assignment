1c5c0ccafbd1a3979744e836cd573fbf4791fd93
Anubhav <anubhavc9@gmail.com>
2022-03-27 14:38:01 +0530

    contact component styling

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactInfo = [
+    {key: 'Phone', value: '+91 88009-69322 / +91 93543-87396', link: ''},
+    {key: 'Email', value: 'anubhavc9@gmail.com', link: ''},
+    {key: 'GitHub', value: 'github.com/anubhavc9', link: 'https://www.github.com/anubhavc9'},
+    {key: 'LinkedIn', value: 'linkedin.com/in/anubhav-kandiyal/', link: 'https://www.linkedin.com/in/anubhav-kandiyal/'},
+];
+  constructor() {}
 
   ngOnInit(): void {
   }
