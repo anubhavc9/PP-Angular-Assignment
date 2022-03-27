f6a050c7a6c4b8ba47687ca485f30f34f29de32a
Anubhav <anubhavc9@gmail.com>
2022-03-27 13:04:37 +0530

    bg changes & contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 
 const routes: Routes = [
   { path: '', component: MainContentComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
