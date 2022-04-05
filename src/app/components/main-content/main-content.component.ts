26f08c1c28aacb7b5a3d559e6b2110038ab715d4
Anubhav <anubhavc9@gmail.com>
2022-04-05 14:26:14 +0530

    home page career obj changed

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
+ 
+  public name: string;
+  public careerObj: string;
 
-  constructor() { }
+  constructor(
+  ) { 
+    this.name = `Anubhav Kandiyal`;
+    this.careerObj = `I'm an aspiring Full-Stack Web Developer looking for an entry-level position in a dynamic organization that values my analytical and technical skills and provides scope for updating my knowledge. I seek a company that will help me contribute to its development while concurrently aiding my personal growth.`
+  }
 
   ngOnInit(): void {
   }
