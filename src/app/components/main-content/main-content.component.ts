b615b90cac7b59c62054a44aa43353f7f385dea6
Anubhav Kandiyal <anubhav.kandiyal@agrostar.in>
2022-07-22 22:17:34 +0530

    updated

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -3,20 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.css']
+  styleUrls: ['./main-content.component.css'],
 })
 export class MainContentComponent implements OnInit {
- 
   public name: string;
-  public careerObj: string;
+  public intro: string;
 
-  constructor(
-  ) { 
+  constructor() {
     this.name = `Anubhav Kandiyal`;
-    this.careerObj = `I'm an aspiring Full-Stack Web Developer looking for an entry-level position in a dynamic organization that values my analytical and technical skills and provides scope for updating my knowledge. I seek a company that will help me contribute to its development while concurrently aiding my personal growth.`
+    // this.intro = `I'm an aspiring Full-Stack Web Developer looking for an entry-level position in a dynamic organization that values my analytical and technical skills and provides scope for updating my knowledge. I seek a company that will help me contribute to its development while concurrently aiding my personal growth.`;
+    this.intro = `I'm a Software Developement Engineer, currently working as a Frontend Developer at AgroStar, Pune. I work on building highly scalable & responsive web apps.`;
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
